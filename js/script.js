99f8d54a3c429b0fdb6a67763e4a5ea39118f150
Typhoonic <koxxor12@gmail.com>
2025-06-08 23:00:16 +0200

    remove all active article from post

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 'use strict';
 
+function removeActiveLinks() {
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+}
+
+function removeActiveArticles() {
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+}
+
+function addActiveToClickedLink(event) {
+    console.log('test');
+}
+
 function titleClickHandler(event) {
     console.log('Link was clicked');
 
-    console.log(event)
-    /* remove class 'active' from all article links  */
+    console.log(event);
+
+    removeActiveLinks();
+    removeActiveArticles();
+
+    // addActiveToClickedLink(event)
 
     /* add class 'active' to the clicked link */
-
-    /* remove class 'active' from all articles */
 
     /* get 'href' attribute from the clicked link */
 
